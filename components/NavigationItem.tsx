1ae6cdf0ebf3e126ae04d8883a562349d4b02f72
Leo Driesch <dev@leodriesch.com>
2023-06-17 18:42:19 +0200

    Refactor navigation item to use CSS hover states

--- components/NavigationItem.tsx
+++ components/NavigationItem.tsx
@@ -1,31 +1,25 @@
-import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
 import { classNames } from "../lib/classNames";
 import { PlaylistInfo } from "../types/playlist-response";
 
 type Props = { item: PlaylistInfo; onNavigate?: () => void };
 
 export function NavigationItem({ item, onNavigate }: Props) {
-  const [isHovered, setIsHovered] = useState(false);
+  const currentPath = usePathname();
 
-  const asPath = usePathname();
+  const playlistId = item.id;
 
-  const path = `/app/playlists/${item.id}`;
-
-  const isActive = asPath === path;
+  const isActive = currentPath === `/app/playlists/${playlistId}`;
 
   return (
     <Link
-      href={path}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
+      href={`/app/playlists/${playlistId}`}
       onClick={onNavigate}
       className={classNames(
         isActive
-          ? "bg-gradient-to-r from-teal-600 to-emerald-500 text-white hover:text-white"
+          ? "list-item-active bg-gradient-to-r from-teal-600 to-emerald-500 text-white hover:text-white"
           : "text-gray-600 hover:text-gray-900",
         "group flex items-center px-3 py-2 text-sm font-medium rounded-md space-x-3 h-12"
       )}
@@ -37,32 +31,33 @@ export function NavigationItem({ item, onNavigate }: Props) {
         height={32}
         className="w-8 h-8 rounded-sm object-cover"
       />
-      <div className="flex flex-col">
-        <AnimatePresence mode="popLayout">
-          <motion.span
-            key="name"
-            className="flex-1"
-            layout="position"
-            transition={{ duration: 0.12 }}
-          >
-            {item.name}
-          </motion.span>
-          {(isHovered || isActive) && (
-            <motion.span
-              key="track-count"
-              className={classNames(
-                "text-xs",
-                isActive ? "text-emerald-200" : "text-gray-400"
-              )}
-              initial={{ scale: 0.8, opacity: 0, y: 10 }}
-              animate={{ scale: 1, opacity: 1, y: 0 }}
-              exit={{ scale: 0.8, opacity: 0, y: 10 }}
-              transition={{ duration: 0.12 }}
-            >
-              {item.tracks.total} tracks
-            </motion.span>
+      <div className="grid">
+        <span
+          className={classNames(
+            "leading-snug",
+            "col-1 col-start-1 col-span-1 row-start-1 row-span-1",
+            "transform translate-y-0 group-hover:-translate-y-1/2 group-[.list-item-active]:-translate-y-1/2",
+            "transition-transform duration-150 ease-in-out"
           )}
-        </AnimatePresence>
+        >
+          {item.name}
+        </span>
+        <span
+          aria-hidden={!isActive}
+          className={classNames(
+            "text-xs leading-snug",
+            "col-1 col-start-1 col-span-1 row-start-1 row-span-1",
+
+            "transform translate-y-full group-hover:translate-y-1/2 group-[.list-item-active]:translate-y-1/2",
+            "opacity-0 group-hover:opacity-100 group-[.list-item-active]:opacity-100",
+            "scale-75 group-hover:scale-100 group-[.list-item-active]:scale-100",
+
+            "transition duration-150 ease-in-out",
+            isActive ? "text-emerald-200" : "text-gray-400"
+          )}
+        >
+          {item.tracks.total} tracks
+        </span>
       </div>
     </Link>
   );
